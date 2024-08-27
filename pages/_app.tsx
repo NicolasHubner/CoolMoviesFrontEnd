c90cad8069523afe000f74cbb67bb64b953837a8
Nicolas H <hubnersantos@aol.com>
2024-08-27 16:44:51 -0300

    [Feat] Add NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,31 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import React, { FC, useState } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
+import type {AppProps} from 'next/app';
+import React, {FC, useState} from 'react';
+import {Provider as ReduxProvider} from 'react-redux';
 import Head from 'next/head';
 import {useApolloClientApi} from "@/apollo";
+import {NavBar} from "@/components/NavBar";
 
-const App: FC<AppProps> = ({ Component, pageProps }) => {
+const App: FC<AppProps> = ({Component, pageProps}) => {
 
-  const store = useApolloClientApi();
+    const store = useApolloClientApi();
 
-  if (!store) return <>{'Loading...'}</>;
+    if (!store) return <>{'Loading...'}</>;
 
-  return (
-    <>
-      <Head>
-        <title>{'Coolmovies Frontend'}</title>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </Head>
-      <ReduxProvider store={store}>
-        <Component {...pageProps} />
-      </ReduxProvider>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>{'Coolmovies Frontend'}</title>
+                <meta charSet='UTF-8'/>
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+            </Head>
+            <ReduxProvider store={store}>
+                <NavBar/>
+                <Component {...pageProps} />
+            </ReduxProvider>
+        </>
+    );
 };
 
 export default App;
